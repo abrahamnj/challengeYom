7769bb92d84f1bc3cb4c28cd35efe4275f352050
abrahamnj <76785113+abrahamnj@users.noreply.github.com>
2024-06-06 02:44:25 -0500

    Update main.py

--- bin/CloudFuction/main.py
+++ bin/CloudFuction/main.py
@@ -1,25 +1,16 @@
-import pandas as pd
-import requests
 from google.cloud import storage
-
-
-def download_csv_from_url(url):
-    response = requests.get(url)
-    df = pd.read_csv(io.StringIO(response.text))
-    return df
-
-def upload_csv_to_gcs(df, bucket_name, destination_blob_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-    df.to_csv('resultado_web.csv', index=False)
-    blob.upload_from_filename('resultado_web.csv')
+import requests
 
 def main(request):
     url = 'https://www.datos.gov.co/resource/avtd-u64r.csv'
-    df = download_csv_from_url(url)
-    bucket_name = 'repositorio_base_personas'
-    destination_blob_name = 'your_destination_blob_name.csv'
-    upload_csv_to_gcs(df, bucket_name, destination_blob_name)
-    return 'CSV file uploaded to Google Cloud Storage'
+    response = requests.get(url)
 
+    # Guarda el archivo en el bucket
+    bucket_name = "repositorio_base_personas"
+    blob_name = "data.csv"
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string(response.content.decode("utf-8"))
+
+    return f"Archivo {blob_name} subido exitosamente al bucket {bucket_name}"
