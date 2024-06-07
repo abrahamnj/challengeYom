6d957669831b2ce38b0a27d929f69b889d6d8a8b
abrahamnj <76785113+abrahamnj@users.noreply.github.com>
2024-06-07 03:59:43 -0500

    Update main.py

--- bin/CloudFuction/main.py
+++ bin/CloudFuction/main.py
@@ -1,23 +1,5 @@
-# from google.cloud import storage
-# import requests
-
-# def main(request):
-#     url = 'https://www.datos.gov.co/resource/avtd-u64r.csv'
-#     response = requests.get(url)
-
-#     # Guarda el archivo en el bucket
-#     bucket_name = "repositorio_base_personas"
-#     blob_name = "data.csv"
-#     storage_client = storage.Client()
-#     bucket = storage_client.bucket(bucket_name)
-#     blob = bucket.blob(blob_name)
-#     blob.upload_from_string(response.content.decode("utf-8"))
-
-#     return f"Archivo {blob_name} subido exitosamente al bucket {bucket_name}"
-
 from google.cloud import storage
 import requests
-import zipfile
 
 def main(request):
     url = 'https://www.datos.gov.co/resource/avtd-u64r.csv'
@@ -25,16 +7,10 @@ def main(request):
 
     # Guarda el archivo en el bucket
     bucket_name = "repositorio_base_personas"
-    blob_name = "base_descargada_web.csv"
+    blob_name = "data.csv"
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
     blob.upload_from_string(response.content.decode("utf-8"))
 
-    # Agregar la parte de código para empaquetar "main.py" en un archivo ZIP llamado "function_code.zip"
-    zip_filename = "function-code.zip"
-    python_filename = "main.py"
-    with zipfile.ZipFile(zip_filename, "w") as zip_file:
-        zip_file.write(python_filename)
-
-    return f"Archivo {blob_name} subido exitosamente al bucket {bucket_name} y {python_filename} empaquetado en {zip_filename}"
+    return f"Archivo {blob_name} subido exitosamente al bucket {bucket_name}"
