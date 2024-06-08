45a75d739699a6703d3a158e741749e9099d8323
abrahamnj <76785113+abrahamnj@users.noreply.github.com>
2024-06-07 22:48:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from google.cloud import storage
 import requests
 
-def obtener_base_web(request):
+def gatillador(request):
     url = 'https://www.datos.gov.co/resource/avtd-u64r.csv'
     response = requests.get(url)
 
     # Guarda el archivo en el bucket
     bucket_name = "repositorio_base_personas"
-    blob_name = "data.csv"
+    blob_name = "base_descargada_web.csv"
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
     blob.upload_from_string(response.content.decode("utf-8"))
+
     return f"Archivo {blob_name} subido exitosamente al bucket {bucket_name}"
+
+
+
+##################################################
