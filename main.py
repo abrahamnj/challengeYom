85837f5cf34b4cf344e2b9df294c22a72b583906
abrahamnj <76785113+abrahamnj@users.noreply.github.com>
2024-06-07 21:48:38 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from google.cloud import storage
+import requests
+
+def obtener_base_web(request):
+    url = 'https://www.datos.gov.co/resource/avtd-u64r.csv'
+    response = requests.get(url)
+
+    # Guarda el archivo en el bucket
+    bucket_name = "repositorio_base_personas"
+    blob_name = "data.csv"
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string(response.content.decode("utf-8"))
+    return f"Archivo {blob_name} subido exitosamente al bucket {bucket_name}"
