fc5e8e2980340fe9b7bd28698a50e66e52f86e4d
abrahamnj <76785113+abrahamnj@users.noreply.github.com>
2024-06-07 23:01:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from google.cloud import storage
 import requests
 
 def gatillador(request):
+    """
+    Esta función descarga un archivo CSV desde una URL y lo sube a un bucket de Google Cloud Storage.
+    :param request: No se utiliza en el código.
+    :return: Mensaje indicando que el archivo se ha subido correctamente al bucket.
+    """
     url = 'https://www.datos.gov.co/resource/avtd-u64r.csv'
     response = requests.get(url)
 
@@ -14,7 +19,3 @@ def gatillador(request):
     blob.upload_from_string(response.content.decode("utf-8"))
 
     return f"Archivo {blob_name} subido exitosamente al bucket {bucket_name}"
-
-
-
-##################################################
